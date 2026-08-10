9511216f8fc453f466a1a4275486f91cb1dfaa8d
HELEN <alenagolosova97@gmail.com>
2026-08-10 23:32:18 +0300

    fix page, css, add key

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,90 @@
+"use client";
+import { useState } from "react";
+const week = [
+  {
+    day: "ПОНЕДІЛОК",
+    date: "10 серпня",
+  },
+  {
+    day: "ВІВТОРОК",
+    date: "11 серпня",
+  },
+  {
+    day: "СЕРЕДА",
+    date: "12 серпня",
+  },
+  {
+    day: "ЧЕТВЕР",
+    date: "13 серпня",
+  },
+  {
+    day: "П'ЯТНИЦЯ",
+    date: "14 серпня",
+  },
+  {
+    day: "СУБОТА",
+    date: "15 серпня",
+  },
+  {
+    day: "НЕДІЛЯ",
+    date: "16 серпня",
+  },
+];
+
+const getSavedNotes = () => {
+  if (typeof window === "undefined") {
+    return [];
+  }
+
+  const savedNotes = localStorage.getItem("notebook-notes");
+
+  return savedNotes ? JSON.parse(savedNotes) : [];
+};
+
 export default function Home() {
+  const [notes, setNotes] = useState<string[]>(getSavedNotes);
+
+  const handleChange = (index: number, value: string) => {
+    const updatedNotes = [...notes];
+
+    updatedNotes[index] = value;
+
+    setNotes(updatedNotes);
+
+    localStorage.setItem("notebook-notes", JSON.stringify(updatedNotes));
+  };
+  let inputIndex = 0;
+
   return (
     <main className="notebook">
-      <section className="day">
-        <h1>ПОНЕДІЛОК</h1>
-        <p className="date">10 серпня</p>
+      <div className="week">
+        {week.map((item, index) => {
+          const lines = index === 6 ? 4 : 6;
+          return (
+            <section className="day" key={item.day}>
+              <h1>{item.day}</h1>
+              <p className="date">{item.date}</p>
 
-        <h2>Плани на сьогодні:</h2>
-
-        <textarea placeholder="Тиць..." />
-
-        <h2>Важливе:</h2>
-
-        <textarea placeholder="І тут тиць..." />
-      </section>
-
-      <section className="day">
-        <h1>ВІВТОРОК</h1>
-        <p className="date">11 серпня</p>
-
-        <h2>Плани на сьогодні:</h2>
-
-        <textarea placeholder="Тиць..." />
-
-        <h2>Важливе:</h2>
-
-        <textarea placeholder="І тут тиць..." />
-      </section>
+              <div className="writing-lines">
+                {Array.from({ length: lines }).map((_, lineIndex) => {
+                  const currentIndex = inputIndex++;
+                  return (
+                    <input
+                      key={lineIndex}
+                      type="text"
+                      className="writing-line"
+                      value={notes[currentIndex] || ""}
+                      onChange={(event) =>
+                        handleChange(currentIndex, event.target.value)
+                      }
+                    />
+                  );
+                })}
+              </div>
+            </section>
+          );
+        })}
+      </div>
     </main>
   );
 }
