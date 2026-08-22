9510a677e8ce385acebc414a06f9adc5f6d7f170
HELEN <alenagolosova97@gmail.com>
2026-08-22 21:30:38 +0300

    add spinner, deleted pen.png

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,27 +1,21 @@
-import styles from "./Loader.module.css";
-import Image from "next/image";
+"use client";
+
+import { useSettingsStore } from "@/lib/store/settingsStore";
+import { FallingLines } from "react-loader-spinner";
+
 export default function Loader({
   size = "small",
 }: {
   size?: "small" | "large";
 }) {
+  const theme = useSettingsStore((state) => state.theme);
+  console.warn("Loader theme:", theme);
+  const width = size === "large" ? "80" : "40";
+  const color = theme === "dark" ? "#f7f1e3" : "#2c2316";
+
   return (
-    <span
-      className={`${styles.loader} ${size === "large" ? styles.loaderLarge : ""}`}
-      role="status"
-      aria-label="Завантаження"
-    >
-      <span className={`${styles.stroke} ${styles.stroke1}`} />
-      <span className={`${styles.stroke} ${styles.stroke2}`} />
-      <span className={`${styles.stroke} ${styles.stroke3}`} />
-      <Image
-        src="/loader/pen.svg"
-        alt=""
-        width={42}
-        height={42}
-        className={styles.pen}
-        priority
-      />
+    <span role="status" aria-label="Завантаження">
+      <FallingLines color={color} width={width} visible={true} />
     </span>
   );
 }
